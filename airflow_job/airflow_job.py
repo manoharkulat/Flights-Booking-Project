3100a29dfefaa0066d4e8a5fafa0778f12b43467
manohar <manoharkulat015@gmail.com>
2026-01-18 14:38:40 +0530

    added new schedule date

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -11,7 +11,7 @@ default_args = {
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 12, 14),
+    'start_date': datetime(2026, 1, 18),
 }
 
 # Define the DAG
